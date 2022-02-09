5a7370f3eea57f88a4994b073ef9213895c77d9f
Raj-nitbhopal <rajankmr117@gmail.com>
2022-02-09 23:19:22 +0530

    Average of Number Program

---
+++ Averageofnumber/Average of number/Module1.vb
@@ -0,0 +1,26 @@
+﻿Imports System
+Namespace Mynamspace
+    Module Module1
+
+        Sub Main()
+            Averageint()
+            Dim intvariable1, Intvariable2, Intvariable3, intAverage As Integer
+            Con     Console.ReadLine()
+
+        End Sub
+        Sub Averageint()
+            Console.WriteLine("****AVERAGE OF INTEGER****")
+            sole.WriteLine("Enter First Number:")
+            intvariable1 = Console.ReadLine()
+            Console.WriteLine("Enter Second Number:")
+            Intvariable2 = Console.ReadLine()
+            Console.WriteLine("Enter Third Number:")
+            Intvariable3 = Console.ReadLine()
+            intAverage = (intvariable1 + Intvariable2 + Intvariable3) / 3
+            Console.WriteLine("Average is above Number={0}.", intAverage)
+
+
+        End Sub
+
+    End Module
+End Namespace
---
+++ Averageofnumber/Average of number/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Average of number")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Average of number")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("db1ebdaa-86ca-4286-87fc-a99d42e96ed8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
